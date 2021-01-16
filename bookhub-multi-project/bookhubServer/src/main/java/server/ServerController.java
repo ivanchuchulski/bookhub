10a8034060ad9da3826fb6d058ac504df7f2b8ea
Viktor Hristov <hristov_98@yahoo.com>
2021-01-16 22:57:44 +0200

    Add basic Server GUI

--- bookhub-multi-project/bookhubServer/src/main/java/server/ServerController.java
+++ bookhub-multi-project/bookhubServer/src/main/java/server/ServerController.java
@@ -3,77 +3,47 @@ package server;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class ServerController {
 
     @FXML
-    private Label lblAccountInfo;
+    private Label lblServerTitle;
 
     @FXML
-    private Label lblUsername;
+    private TextArea txaUsers;
 
     @FXML
-    private Label lblPassword;
+    private TextArea txaBooks;
 
     @FXML
-    private Label lblPrivilege;
+    private Button btnDisplayBooks;
 
     @FXML
-    private TextField txtUsername;
+    private Button btnDisplayUsers;
 
     @FXML
-    private TextField txtPassword;
+    private Button btnStopServer;
 
     @FXML
-    private ComboBox<?> cmbPrivilege;
+    private Label lblUsers;
 
     @FXML
-    private Label lblViewData;
+    private Label lblBooks;
 
     @FXML
-    private TextArea txaLog;
-
-    @FXML
-    private Button btnOpenByEncryption;
-
-    @FXML
-    private Button btnOpenByCard;
-
-    @FXML
-    private Label lblTableView;
-
-    @FXML
-    private TableView<?> tbvTableView;
-
-    @FXML
-    private Button btnViewUsers;
-
-    @FXML
-    private Button btnViewCardNumbers;
-
-    @FXML
-    void btnOpenByCardClicked(ActionEvent event) {
+    void btnDisplayBooksClicked(ActionEvent event) {
 
     }
 
     @FXML
-    void btnOpenByEncryptionClicked(ActionEvent event) {
+    void btnDisplayUsersClicked(ActionEvent event) {
 
     }
 
     @FXML
-    void btnViewCardNumbersClicked(ActionEvent event) {
+    void btnStopServerClicked(ActionEvent event) {
 
     }
-
-    @FXML
-    void btnViewUsersClicked(ActionEvent event) {
-
-    }
-
 }
